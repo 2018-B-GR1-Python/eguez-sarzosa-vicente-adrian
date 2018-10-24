e54acd1c66deb63943c583e29f63ea73f9469fb8
Vicente Eguez <eadepto@hotmail.com>
2018-10-24 09:14:39 -0500

    importacion actualizacion

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from modulouno import modulo
+from modulouno.modulo import modulo_dos, modulo_uno
 from modulouno.modulounouno import modulouu
 from inicializar import init, init_dos
 
-modulo.modulo_uno()
-modulo.modulo_dos()
+modulo_uno()
+modulo_dos()
 modulouu.modulo_dos()
 init()
